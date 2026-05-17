9dd3803af9bc64662ec007356ad5593f718b45a3
marceleazoliveira <marceleazeveoli@gmail.com>
2026-05-16 21:50:08 -0400

    Create 21_2.pl

---
+++ 21_2.pl
@@ -0,0 +1,58 @@
+questao_21_2(Steps, HipoteseFinal):-
+
+    % Passo 0: hipótese inicial
+    H0 = [
+        [predecessor(A,B)]/[A,B],
+        [predecessor(D,E)]/[D,E]
+    ],
+
+    % 1: adiciona atom(A), parent(A,C) na primeira cláusula
+    H1 = [
+        [predecessor(A,B), [atom(A), parent(A,C)]]/[A,B,A,C],
+        [predecessor(D,E)]/[D,E]
+    ],
+
+    % 2: remove variável repetida A
+    H2 = [
+        [predecessor(A,B), [atom(A), parent(A,C)]]/[A,B,C],
+        [predecessor(D,E)]/[D,E]
+    ],
+
+    % 3: adiciona atom(C), predecessor(C,B)
+    H3 = [
+        [predecessor(A,B), [atom(A), parent(A,C)], [atom(C), predecessor(C,B)]]/[A,B,C,C,B],
+        [predecessor(D,E)]/[D,E]
+    ],
+
+    % 4: remove variável repetida C
+    H4 = [
+        [predecessor(A,B), [atom(A), parent(A,C)], [atom(C), predecessor(C,B)]]/[A,B,C,B],
+        [predecessor(D,E)]/[D,E]
+    ],
+
+    % 5: remove variável repetida B
+    H5 = [
+        [predecessor(A,B), [atom(A), parent(A,C)], [atom(C), predecessor(C,B)]]/[A,B,C],
+        [predecessor(D,E)]/[D,E]
+    ],
+
+    % 6: adiciona atom(D), parent(D,E) na segunda cláusula
+    H6 = [
+        [predecessor(A,B), [atom(A), parent(A,C)], [atom(C), predecessor(C,B)]]/[A,B,C],
+        [predecessor(D,E), [atom(D), parent(D,E)]]/[D,E,D,E]
+    ],
+
+    % 7: remove variável repetida D
+    H7 = [
+        [predecessor(A,B), [atom(A), parent(A,C)], [atom(C), predecessor(C,B)]]/[A,B,C],
+        [predecessor(D,E), [atom(D), parent(D,E)]]/[D,E,E]
+    ],
+
+    % 8: remove variável repetida E
+    H8 = [
+        [predecessor(A,B), [atom(A), parent(A,C)], [atom(C), predecessor(C,B)]]/[A,B,C],
+        [predecessor(D,E), [atom(D), parent(D,E)]]/[D,E]
+    ],
+
+    Steps = 8,
+    HipoteseFinal = H8.
